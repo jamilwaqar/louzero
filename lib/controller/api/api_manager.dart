53d2f5781bf9c8319dcf12df74e454399cd4056e
Mark Austen <austenmark8@gmail.com>
2021-10-19 20:53:19 +0200

    UDE-101: [MOB] [IMPL] Configure Firebase project and Flutter project

---
+++ lib/controller/api/api_manager.dart
@@ -0,0 +1,6 @@
+class APIManager {
+  static const API_HOST = "https://api.backendless.com";
+  static const APPLICATION_ID = "D8545E9B-1856-3BFE-FF33-600B71DE0300";
+  static const ANDROID_API_KEY = "AD89DC13-EF4C-4020-AF1D-F33BE922DA3F";
+  static const IOS_API_KEY = "1B0D4E55-CBBE-48F2-9983-D177CC415326";
+}
