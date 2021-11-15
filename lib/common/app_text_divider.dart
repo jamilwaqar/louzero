9a4026d563101c636bee8f785f7e01d7f8ae2a6e
Josh C, UX Developer <josh.coolman@singlemindconsulting.com>
2021-11-15 10:20:35 -0800

    common

---
+++ lib/common/app_text_divider.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:louzero/controller/constant/colors.dart';
+
+class AppTextDivider extends StatelessWidget {
+  const AppTextDivider(
+      {Key? key, this.label = 'or', this.mt = 24, this.mb = 24})
+      : super(key: key);
+
+  final String label;
+  final double mt;
+  final double mb;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.only(top: mt, bottom: mt),
+      child: Row(
+        children: [
+          Expanded(
+            child: Container(
+              height: 1,
+              color: AppColors.light_3,
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 24),
+            child: Text(label,
+                style: const TextStyle(
+                  color: AppColors.dark_1,
+                  fontWeight: FontWeight.w400,
+                  fontSize: 16,
+                )),
+          ),
+          Expanded(
+            child: Container(
+              height: 1,
+              color: AppColors.light_3,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
