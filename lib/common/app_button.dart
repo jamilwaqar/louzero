abd62d22c00417094ec95f796cb4c2145fb661fb
Josh C, UX Developer <josh.coolman@singlemindconsulting.com>
2021-11-17 10:47:58 -0800

    refactor app button

--- lib/common/app_button.dart
+++ lib/common/app_button.dart
@@ -4,70 +4,63 @@ import 'package:flutter/material.dart';
 import 'package:louzero/controller/constant/colors.dart';
 
 class AppButton extends StatelessWidget {
-  AppButton({
+  const AppButton({
     Key? key,
     this.label = 'button',
     this.onPressed,
+    this.radius = 999,
     this.icon,
     this.primary = true,
-    this.mb = 0,
-    this.mt = 0,
+    this.color = AppColors.dark_3,
+    this.colorText = AppColors.lightest,
+    // Spacing props:
+    this.mb = 0, // margin bottom
+    this.mt = 0, // margin top
+    this.ml = 0, // margin left
+    this.mr = 0, // margin right
   }) : super(key: key);
 
   final VoidCallback? onPressed;
   final String label;
   final IconData? icon;
   final bool primary;
+  final Color color;
+  final Color colorText;
+  final double radius;
   final double mt;
   final double mb;
-
-  final BoxDecoration secondaryBox = BoxDecoration(
-      color: AppColors.light_1,
-      borderRadius: BorderRadius.circular(28),
-      border: Border.all(color: AppColors.medium_1, width: 1));
-
-  final BoxDecoration primaryBox = BoxDecoration(
-      color: AppColors.black, borderRadius: BorderRadius.circular(28));
-
-  final TextStyle primaryText = const TextStyle(
-    color: AppColors.lightest,
-    fontWeight: FontWeight.w400,
-    fontSize: 16,
-  );
-
-  final TextStyle secondaryText = const TextStyle(
-    color: AppColors.dark_1,
-    fontWeight: FontWeight.w500,
-    fontSize: 16,
-  );
+  final double ml;
+  final double mr;
 
   @override
   Widget build(BuildContext context) {
-    return CupertinoButton(
-      padding: EdgeInsets.zero,
-      onPressed: onPressed ?? () => {},
-      child: Container(
-        height: 56,
-        alignment: Alignment.center,
-        margin: EdgeInsets.only(top: mt, bottom: mb),
-        decoration: primary ? primaryBox : secondaryBox,
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Visibility(
-              visible: icon != null ? true : false,
-              child: Icon(
-                icon,
-                color: AppColors.light_2,
-              ),
-            ),
-            const SizedBox(
-              width: 8,
-            ),
-            Text(label, style: primary ? primaryText : secondaryText),
-          ],
-        ),
-      ),
+    var fg = primary ? colorText : color;
+    var bg = primary ? color : AppColors.lightest;
+    var bd = primary ? color : color;
+    const textStyle = TextStyle(
+        fontFamily: 'Roboto', fontWeight: FontWeight.w500, fontSize: 16);
+
+    return Flex(
+      direction: Axis.horizontal,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Expanded(
+          child: Container(
+            margin: EdgeInsets.only(top: mt, bottom: mb),
+            child: FloatingActionButton.extended(
+                foregroundColor: fg,
+                backgroundColor: bg,
+                icon: icon != null ? Icon(icon) : null,
+                elevation: 0,
+                extendedTextStyle: textStyle,
+                shape: RoundedRectangleBorder(
+                    side: BorderSide(color: bd, width: 1.0),
+                    borderRadius: BorderRadius.all(Radius.circular(radius))),
+                onPressed: onPressed,
+                label: Text(label)),
+          ),
+        )
+      ],
     );
   }
 }
