b5df422c7fb574e960b6eb10a5eb954cd4460a3b
Josh C, UX Developer <josh.coolman@singlemindconsulting.com>
2021-11-17 18:32:01 -0800

    new components

--- lib/common/app_card.dart
+++ lib/common/app_card.dart
@@ -7,31 +7,89 @@ class AppCard extends StatelessWidget {
     Key? key,
     required this.child,
     this.width = 0,
+    this.pt = 0,
+    this.pb = 0,
+    this.pl = 0,
+    this.pr = 0,
     this.px = 0,
     this.py = 0,
-    this.mx = 0,
-    this.my = 0,
+    this.p = 0,
     this.mt = 0,
     this.mb = 0,
     this.ml = 0,
     this.mr = 0,
+    this.mx = 0,
+    this.my = 0,
+    this.m = 0,
   }) : super(key: key);
   final Widget child;
   final double width;
+  // spacing props (margin / padding)
+  final double pt;
+  final double pb;
+  final double pl;
+  final double pr;
+  final double px;
+  final double py;
+  final double p;
   final double mt;
   final double mb;
   final double ml;
   final double mr;
-  final double px;
-  final double py;
   final double mx;
   final double my;
+  final double m;
 
   @override
   Widget build(BuildContext context) {
+    var margin = EdgeInsets.only(
+      top: m > 0
+          ? m
+          : my > 0
+              ? my
+              : mt,
+      left: m > 0
+          ? m
+          : mx > 0
+              ? mx
+              : ml,
+      right: m > 0
+          ? m
+          : mx > 0
+              ? mx
+              : ml,
+      bottom: m > 0
+          ? m
+          : my > 0
+              ? my
+              : mt,
+    );
+    var padding = EdgeInsets.only(
+      top: p > 0
+          ? p
+          : py > 0
+              ? py
+              : pt,
+      left: p > 0
+          ? p
+          : px > 0
+              ? px
+              : pl,
+      right: p > 0
+          ? p
+          : px > 0
+              ? px
+              : pr,
+      bottom: p > 0
+          ? p
+          : py > 0
+              ? py
+              : pb,
+    );
+
     return Container(
-      margin: EdgeInsets.only(top: mt, left: ml, right: mr, bottom: mb),
-      padding: EdgeInsets.only(top: py, left: px, right: px, bottom: py),
+      margin: margin,
+      padding: padding,
       decoration: BoxDecorationEx.shadowEffect(
           borderRadius: BorderRadius.circular(8),
           blurRadius: 3,
---
+++ lib/ui/page/account/account_start.dart
@@ -0,0 +1,182 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:louzero/common/app_card.dart';
+import 'package:louzero/common/app_text_body.dart';
+import 'package:louzero/common/app_text_header.dart';
+import 'package:louzero/controller/constant/colors.dart';
+import 'package:louzero/ui/page/base_scaffold.dart';
+
+class AccountStart extends StatefulWidget {
+  const AccountStart({Key? key}) : super(key: key);
+
+  @override
+  _AccountStartState createState() => _AccountStartState();
+}
+
+class _AccountStartState extends State<AccountStart> {
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return BaseScaffold(
+      child: Column(
+        children: [
+          Expanded(
+              flex: 1,
+              child: Column(
+                children: [
+                  const AppTextHeader(
+                    "To start, let’s get some basic info.",
+                    mt: 32,
+                    mb: 8,
+                  ),
+                  const AppTextBody(
+                    'You can always make changes later in Settings',
+                    mb: 32,
+                  ),
+                  StepProgress()
+                ],
+              )),
+          Expanded(
+            flex: 3,
+            child: PageView(children: const [
+              AppCard(m: 24, p: 24, child: AppTextHeader('Step One')),
+              AppCard(m: 24, p: 24, child: AppTextHeader('Step Two')),
+              AppCard(m: 24, p: 24, child: AppTextHeader('Step Three')),
+            ]),
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class StepProgress extends StatelessWidget {
+  StepProgress({Key? key}) : super(key: key);
+  final _key = GlobalKey();
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      key: _key,
+      children: [
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: const [
+            StepNumberDash(stepNumber: 1, startSize: 0, complete: true),
+            StepNumberDash(stepNumber: 2),
+            StepNumberDash(stepNumber: 3),
+            StepNumberDash(stepNumber: 4, endSize: 0),
+          ],
+        ),
+      ],
+    );
+  }
+}
+
+class StepNumberDash extends StatelessWidget {
+  const StepNumberDash(
+      {Key? key,
+      this.stepNumber = 0,
+      this.stepSize = 40,
+      this.dashSize = 40,
+      this.endSize = 20,
+      this.startSize = 20,
+      this.selected = false,
+      this.complete = false})
+      : super(key: key);
+
+  final int stepNumber;
+  final double dashSize;
+  final double stepSize;
+  final double startSize;
+  final double endSize;
+  final bool selected;
+  final bool complete;
+  final IconData icon = Icons.check;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            StepDash(
+              size: startSize != 20 ? startSize : dashSize,
+            ),
+            StepNumber(
+              stepNumber,
+              size: stepSize,
+              icon: complete ? icon : null,
+            ),
+            StepDash(
+              size: endSize != 20 ? endSize : dashSize,
+            ),
+          ],
+        ),
+      ],
+    );
+  }
+}
+
+class StepNumber extends StatelessWidget {
+  const StepNumber(this.step,
+      {Key? key,
+      this.size = 50,
+      this.color = AppColors.lightest,
+      this.colorBorder = AppColors.medium_1,
+      this.colorText = AppColors.black,
+      this.icon})
+      : super(key: key);
+  final int step;
+  final double size;
+  final Color color;
+  final Color colorBorder;
+  final Color colorText;
+  final IconData? icon;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Container(
+          width: size,
+          height: size,
+          decoration: BoxDecoration(
+              color: color,
+              shape: BoxShape.circle,
+              border: Border.all(color: colorBorder, width: 2)),
+          child: Center(
+            child: icon != null
+                ? Icon(icon)
+                : Text(
+                    step.toString(),
+                    style: TextStyle(fontSize: size * 0.45, color: colorText),
+                  ),
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class StepDash extends StatelessWidget {
+  const StepDash({Key? key, this.size = 50, this.color = AppColors.medium_1})
+      : super(key: key);
+  final double size;
+  final Color color;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 2,
+      width: size,
+      color: color,
+    );
+  }
+}
