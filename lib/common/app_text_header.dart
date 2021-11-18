6aea323bee55bc2ca1e33dc611f02c3c7df6cd32
Josh C, UX Developer <josh.coolman@singlemindconsulting.com>
2021-11-17 22:12:47 -0800

    add icon / align props

--- lib/common/app_text_header.dart
+++ lib/common/app_text_header.dart
@@ -6,31 +6,55 @@ import 'package:flutter/material.dart';
 import 'package:louzero/controller/constant/colors.dart';
 
 class AppTextHeader extends StatelessWidget {
+  const AppTextHeader(
+    this.text, {
+    Key? key,
+    this.size = 33.0,
+    this.color = AppColors.dark_2,
+    this.mt = 0.0,
+    this.mb = 16,
+    this.alignLeft = false,
+    this.icon,
+  }) : super(key: key);
+
+  final IconData? icon;
   final String text;
   final double size;
   final Color color;
   final double mt; // margin top (defalt:0)
   final double mb; // margin bottom (default:20)
-
-  const AppTextHeader(this.text,
-      {Key? key,
-      this.size = 33.0,
-      this.color = AppColors.dark_2,
-      this.mt = 0.0,
-      this.mb = 16})
-      : super(key: key);
+  final bool alignLeft;
 
   @override
   Widget build(BuildContext context) {
     return Container(
       margin: EdgeInsets.only(top: mt, bottom: mb),
-      child: Text(text,
-          style: TextStyle(
-            fontSize: size,
-            color: color,
-            fontFamily: 'Roboto',
-            letterSpacing: -0.75,
-          )),
+      child: Row(
+        mainAxisSize: MainAxisSize.max,
+        mainAxisAlignment:
+            alignLeft ? MainAxisAlignment.start : MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Visibility(
+            visible: icon != null ? true : false,
+            child: SizedBox(
+              height: size,
+              child: Icon(
+                icon,
+                size: size,
+              ),
+            ),
+          ),
+          SizedBox(width: size * 0.35),
+          Text(text,
+              style: TextStyle(
+                fontSize: size,
+                color: color,
+                fontFamily: 'Roboto',
+                letterSpacing: -0.75,
+              )),
+        ],
+      ),
     );
   }
 }
