718a933735ee7a862faf4dc97288f4c3607d4aed
Josh C, UX Developer <josh.coolman@singlemindconsulting.com>
2021-11-29 10:51:53 -0800

    initial charting widget

---
+++ lib/ui/page/dashboard/chart.dart
@@ -0,0 +1,37 @@
+import 'package:fl_chart/fl_chart.dart';
+import 'package:flutter/material.dart';
+import 'chart_list_item.dart';
+
+double _width = 25;
+
+class Chart extends StatelessWidget {
+  const Chart({Key? key, required this.items}) : super(key: key);
+
+  final List<chartListItem> items;
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 120,
+      child: Stack(
+        children: [
+          PieChart(
+            PieChartData(
+              sectionsSpace: 0,
+              centerSpaceRadius: 30,
+              startDegreeOffset: -90,
+              sections: items.map((item) {
+                return PieChartSectionData(
+                  color: item.color,
+                  value: item.amount,
+                  showTitle: false,
+                  radius: _width,
+                );
+              }).toList(),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
--- lib/ui/page/dashboard/dashboard.dart
+++ lib/ui/page/dashboard/dashboard.dart
@@ -1,12 +1,60 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:louzero/bloc/base/base.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:louzero/common/app_button.dart';
+import 'package:louzero/controller/constant/colors.dart';
 import 'package:louzero/controller/page_navigation/navigation_controller.dart';
 import 'package:louzero/ui/page/base_scaffold.dart';
 import 'package:louzero/ui/page/customer/customers.dart';
 
+import 'app_card_chart_pie.dart';
+import 'chart_list_item.dart';
+
 class DashboardPage extends StatefulWidget {
-  const DashboardPage({Key? key}) : super(key: key);
+  DashboardPage({Key? key}) : super(key: key);
+
+  final List<chartListItem> chartDataJobs = [
+    chartListItem(
+      title: 'Booked Solid',
+      color: AppColors.dark_2,
+      subtitle: '(27)',
+      amount: 70,
+    ),
+    chartListItem(
+      title: 'Estimate',
+      color: AppColors.medium_3,
+      subtitle: '(20)',
+      amount: 20,
+    ),
+    chartListItem(
+      title: 'Invoiced',
+      color: AppColors.light_3,
+      subtitle: '(7 this month)',
+      amount: 10,
+    ),
+    chartListItem(
+      title: 'Canceled',
+      color: AppColors.light_3.withOpacity(0.5),
+      subtitle: '(2 this month)',
+      amount: 5,
+    )
+  ];
+
+  final List<chartListItem> chartDataCustomer = [
+    chartListItem(
+      title: 'Total Customers',
+      color: AppColors.dark_2,
+      subtitle: '(172)',
+      amount: 30,
+    ),
+    chartListItem(
+      title: 'Active this Month',
+      color: AppColors.medium_3,
+      subtitle: '(34)',
+      amount: 60,
+    ),
+  ];
 
   @override
   _DashboardPageState createState() => _DashboardPageState();
@@ -29,12 +77,30 @@ class _DashboardPageState extends State<DashboardPage> {
         padding: EdgeInsets.only(left: margin, right: margin, top: margin * 2),
         child: Row(
           mainAxisSize: MainAxisSize.min,
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             // COLUMN ONE
             Flexible(
               child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
                 children: [
-                  Image.asset('assets/mocks/jobs-card.png'),
+                  AppCardChartPie(
+                    items: widget.chartDataJobs,
+                    title: 'Jobs',
+                    footer: const [
+                      AppButton(
+                        mr: 8,
+                        label: 'Add Job',
+                        color: AppColors.dark_1,
+                        colorText: AppColors.darkest,
+                        primary: false,
+                      ),
+                      AppButton(
+                        color: AppColors.dark_1,
+                        label: 'Search Jobs',
+                      )
+                    ],
+                  ),
                   SizedBox(height: spacing),
                   Image.asset('assets/mocks/reports-card.png'),
                   SizedBox(height: spacing),
@@ -46,15 +112,30 @@ class _DashboardPageState extends State<DashboardPage> {
             // COLUMN TWO
             Flexible(
               child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
                 children: [
                   Image.asset('assets/mocks/schedule-card.png'),
                   SizedBox(height: spacing),
-                  GestureDetector(
-                    child: Image.asset('assets/mocks/customer-card.png'),
-                    onTap: () {
-                      NavigationController()
-                          .pushTo(context, child: const CustomerListPage());
-                    },
+                  AppCardChartPie(
+                    items: widget.chartDataCustomer,
+                    title: 'Customers',
+                    footer: [
+                      const AppButton(
+                        mr: 8,
+                        label: 'Add Customer',
+                        color: AppColors.dark_1,
+                        colorText: AppColors.darkest,
+                        primary: false,
+                      ),
+                      AppButton(
+                        color: AppColors.dark_1,
+                        label: 'View All',
+                        onPressed: () {
+                          NavigationController()
+                              .pushTo(context, child: const CustomerListPage());
+                        },
+                      )
+                    ],
                   ),
                   SizedBox(height: spacing),
                   Image.asset('assets/mocks/inventory-card.png')
