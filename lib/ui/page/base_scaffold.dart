c063628e9e46b4316124a20cbf737aeae1bd3cbf
Josh C, UX Developer <josh.coolman@singlemindconsulting.com>
2021-12-14 08:30:02 -0800

    add logout, cleanup

--- lib/ui/page/base_scaffold.dart
+++ lib/ui/page/base_scaffold.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:get_storage/get_storage.dart';
 import 'package:louzero/common/app_avatar.dart';
+import 'package:louzero/controller/api/auth/auth.dart';
 import 'package:louzero/controller/constant/colors.dart';
+import 'package:louzero/controller/constant/constants.dart';
 import 'package:louzero/controller/page_navigation/navigation_controller.dart';
 import 'package:louzero/controller/state/auth_state.dart';
 import 'package:louzero/ui/widget/side_menu/side_menu.dart';
@@ -18,33 +21,15 @@ class BaseScaffold extends StatefulWidget {
 
 class _BaseScaffoldState extends State<BaseScaffold> {
   final GlobalKey<ScaffoldState> _key = GlobalKey(); // Create a key
+  final navigatorKey = GlobalKey<NavigatorState>();
 
-  List<PopMenuItem> items = [
-    PopMenuItem(
-        label: 'My Account',
-        icon: Icons.person_rounded,
-        onTap: () {
-          print("account");
-        }),
-    PopMenuItem(
-        label: 'Settings',
-        icon: Icons.settings,
-        onTap: () {
-          print("settings");
-        }),
-    PopMenuItem(
-        label: 'Account Setup',
-        icon: MdiIcons.briefcase,
-        onTap: () {
-          print("settings");
-        }),
-    PopMenuItem(
-        label: 'Log Out',
-        icon: Icons.exit_to_app,
-        onTap: () {
-          print("exit");
-        }),
-  ];
+  void _logout() async {
+    GetStorage().write(GSKey.isAuthUser, false);
+    NavigationController().loading();
+    await AuthAPI().logout();
+    NavigationController().loading(isLoading: false);
+    AuthStateManager().loggedIn.value = false;
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -66,26 +51,53 @@ class _BaseScaffoldState extends State<BaseScaffold> {
                     height: 64,
                     child: Image.asset("assets/icons/general/logo_icon.png"),
                   ),
-                  leading: IconButton(
-                    onPressed: () {
-                      _key.currentState?.openDrawer();
-                    },
-                    icon: const Icon(Icons.menu),
-                  ), // Set menu icon at leading of AppBar
+                  leading: isLoggedIn
+                      ? IconButton(
+                          onPressed: () {
+                            _key.currentState?.openDrawer();
+                          },
+                          icon: const Icon(Icons.menu),
+                        )
+                      : null, // Set menu icon at leading of AppBar
                   actions: [
-                    AppPopMenu(
-                      items: items,
-                      button: const [
-                        AppAvatar(
-                          path: 'assets/mocks/profile_corey_2.png',
-                          size: 40,
-                          borderColor: AppColors.lightest,
-                        ),
-                        Icon(Icons.arrow_drop_down, color: AppColors.lightest)
-                      ],
-                    )
+                    if (isLoggedIn)
+                      AppPopMenu(
+                        items: [
+                          PopMenuItem(
+                              label: 'My Account',
+                              icon: Icons.person_rounded,
+                              onTap: () {
+                                print("account");
+                              }),
+                          PopMenuItem(
+                              label: 'Settings',
+                              icon: Icons.settings,
+                              onTap: () {
+                                print("settings");
+                              }),
+                          PopMenuItem(
+                              label: 'Account Setup',
+                              icon: MdiIcons.briefcase,
+                              onTap: () {
+                                print("settings");
+                              }),
+                          PopMenuItem(
+                            label: 'Log Out',
+                            icon: Icons.exit_to_app,
+                            onTap: _logout,
+                          )
+                        ],
+                        button: const [
+                          AppAvatar(
+                            path: 'assets/mocks/profile_corey_2.png',
+                            size: 40,
+                            borderColor: AppColors.lightest,
+                          ),
+                          Icon(Icons.arrow_drop_down, color: AppColors.lightest)
+                        ],
+                      )
                   ]),
-              drawer: const SideMenuView(),
+              drawer: isLoggedIn ? SideMenuView() : null,
               body: Container(
                 color: const Color(0xFFF1F3F5),
                 child: widget.child,
