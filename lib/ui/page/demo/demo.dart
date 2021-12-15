1d2427d35bb79d7a4682475bcccd8e050f97df5d
Josh C, UX Developer <josh.coolman@singlemindconsulting.com>
2021-12-15 14:50:50 -0800

    component playground (keep for testing)

--- lib/ui/page/demo/demo.dart
+++ lib/ui/page/demo/demo.dart
@@ -1,29 +1,109 @@
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
 import 'package:louzero/common/app_button.dart';
 import 'package:louzero/common/app_card.dart';
+import 'package:louzero/common/app_card_tabs.dart';
+import 'package:louzero/common/app_flex_row.dart';
 import 'package:louzero/common/app_input_text.dart';
 import 'package:louzero/common/app_multiselect.dart';
 import 'package:louzero/common/app_pop_menu.dart';
+import 'package:louzero/common/app_spinner.dart';
+import 'package:louzero/common/app_text_body.dart';
 import 'package:louzero/common/app_text_header.dart';
 import 'package:louzero/controller/constant/colors.dart';
 import 'package:louzero/ui/page/base_scaffold.dart';
+import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
 
 class demo extends StatelessWidget {
   demo({Key? key}) : super(key: key);
 
+  @override
+  Widget build(BuildContext context) {
+    return BaseScaffold(
+      child: SingleChildScrollView(
+        child: Column(
+          children: [
+            _tabs(),
+            _loadingSpinner(),
+            _multiSelect(),
+            _formInputs(),
+            _buttonsAndMenus(),
+          ],
+        ),
+      ),
+    );
+  }
+
+  // MOCK DATA (MULTISELECT)
   List<SelectItem> selectItems = [
-    SelectItem(id: '1', value: '', label: 'One'),
-    SelectItem(id: '2', value: '', label: 'Two'),
-    SelectItem(id: '3', value: '', label: 'Three'),
-    SelectItem(id: '4', value: '', label: 'Four'),
-    SelectItem(id: '5', value: '', label: 'Five'),
+    SelectItem(id: '1', value: '', label: 'Cheese'),
+    SelectItem(id: '2', value: '', label: 'Mushrooms'),
+    SelectItem(id: '3', value: '', label: 'Jalepenos'),
+    SelectItem(id: '4', value: '', label: 'Tomatos'),
+    SelectItem(id: '4', value: '', label: 'Peperoni'),
+    SelectItem(id: '4', value: '', label: 'Cookie Dough'),
+    SelectItem(id: '4', value: '', label: 'Sausage'),
+    SelectItem(id: '5', value: '', label: 'Onions'),
+    SelectItem(id: '5', value: '', label: 'Garlic'),
+    SelectItem(id: '5', value: '', label: 'Gravel'),
   ];
 
-  List<Widget> tabItems = [
+  // MOCK DATA (TABS)
+  final List<Widget> tabItems = [
     Container(
       color: AppColors.lightest,
-      child: Icon(Icons.airplane_ticket, size: 150, color: AppColors.orange),
+      child: Container(
+        child: Padding(
+          padding: EdgeInsets.all(24),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              AppTextHeader('Basic Info',
+                  alignLeft: true, icon: Icons.airplane_ticket, size: 24),
+              AppFlexRow(
+                flex: [2, 2],
+                children: [
+                  AppInputText(
+                    label: 'First',
+                    initial: 'Brad',
+                  ),
+                  AppInputText(
+                    label: 'Last',
+                    initial: 'Smith',
+                  ),
+                  AppInputText(
+                    label: 'Alias',
+                    initial: 'The Closer',
+                  ),
+                ],
+              ),
+              SizedBox(
+                height: 16,
+              ),
+              AppFlexRow(
+                flex: [2],
+                children: [
+                  Column(
+                    children: [
+                      AppTextBody(
+                        'What if you want to call it from a stateless widget? Well, that’s possible too. Use a stateful widget as a your root widget that you can provide a callback function too to execute your startup logic. See example below.',
+                        bold: true,
+                        mb: 16,
+                      ),
+                      AppTextBody(
+                        'What if you want to call it from a stateless widget? Well, that’s possible too. Use a stateful widget as a your root widget that you can provide a callback function too to execute your startup logic. See example below.',
+                      )
+                    ],
+                  ),
+                  AppTextBody(
+                    'What if you want to call it from a stateless widget? Well, that’s possible too. Use a stateful widget as a your root widget that you can provide a callback function too to execute your startup logic. See example below.',
+                    pl: 8,
+                  ),
+                ],
+              )
+            ],
+          ),
+        ),
+      ),
     ),
     Container(
       color: AppColors.lightest,
@@ -35,25 +115,7 @@ class demo extends StatelessWidget {
     ),
   ];
 
-  @override
-  Widget build(BuildContext context) {
-    return BaseScaffold(
-      child: SingleChildScrollView(
-        child: Column(
-          children: [
-            AppCardTabs(
-              length: 3,
-              tabNames: ['One', 'Schedule', 'Billing'],
-              children: tabItems,
-            ),
-            _multiSelect(),
-            _buttonsAndMenus(),
-          ],
-        ),
-      ),
-    );
-  }
-
+  // RENDER FUNCTIONS
   Widget _heading(String text,
       [icon = Icons.chevron_right, double px = 0, double py = 0]) {
     return Padding(
@@ -67,37 +129,69 @@ class demo extends StatelessWidget {
     );
   }
 
-  Widget _multiSelect() => AppCard(mt: 24, children: [
+  Widget _tabs() => AppCardTabs(
+        height: 500,
+        length: 3,
+        tabNames: ['Overview', 'Schedule', 'Billing'],
+        children: tabItems,
+      );
+
+  Widget _loadingSpinner() => AppCard(pb: 48, children: [
+        _heading('Loading Spinner', MdiIcons.pirate),
+        const AppSpinner(),
+      ]);
+
+  Widget _multiSelect() => AppCard(children: [
         _heading('MultiSelect Widget', Icons.list),
         AppMultiSelect(
           items: selectItems,
         ),
-        AppInputRow(
-          flex: [2, 1, 1],
+      ]);
+
+  Widget _formInputs() => AppCard(children: [
+        _heading('Form Inputs', Icons.forum_rounded),
+        AppFlexRow(
           children: [
-            AppInputText(label: 'Name', mb: 0, mt: 24),
-            AppInputText(label: 'Phone', mb: 0, mt: 24),
-            AppInputText(label: 'Email', mb: 0, mt: 24)
+            AppInputText(label: 'First'),
+            AppInputText(label: 'Last'),
+            AppInputText(label: 'Nickname'),
           ],
         ),
-        Row(
+        AppFlexRow(
+          flex: [4, 1],
           children: [
-            Expanded(
-              child: AppInputText(label: 'Name', mb: 0, mt: 24),
-            ),
-            SizedBox(width: 16),
-            Expanded(
-              child: AppInputText(label: 'Phone', mb: 0, mt: 24),
-            ),
-            SizedBox(width: 16),
-            Expanded(
-              child: AppInputText(label: 'Phone', mb: 0, mt: 24),
+            AppInputText(label: 'Address'),
+            AppInputText(label: 'Suite'),
+          ],
+        ),
+        AppFlexRow(
+          children: [
+            AppInputText(label: 'Country'),
+            AppMultiSelect(
+              label: 'Toppings',
+              items: selectItems,
             )
+          ],
+        ),
+        AppFlexRow(
+          flex: [2, 3],
+          children: [
+            AppInputText(label: 'Alias'),
+            AppInputText(label: 'Planet of Origin'),
+          ],
+        ),
+        AppFlexRow(
+          flex: [3],
+          children: [
+            Container(),
+            AppButton(
+                label: 'Cancel', primary: false, color: AppColors.secondary_60),
+            AppButton(label: 'Submit', color: AppColors.orange),
           ],
         )
       ]);
 
-  Widget _buttonsAndMenus() => AppCard(mt: 24, children: [
+  Widget _buttonsAndMenus() => AppCard(children: [
         _heading('Buttons and Menus', Icons.control_point_rounded),
         Row(
           children: const [
@@ -133,93 +227,4 @@ class demo extends StatelessWidget {
           ],
         )
       ]);
-}
-
-class AppInputRow extends StatelessWidget {
-  AppInputRow(
-      {Key? key, this.children = const <Widget>[], this.flex = const <int>[]})
-      : super(key: key);
-
-  final List<int> flex;
-  final List<Widget> children;
-  List<Widget> items = [];
-
-  @override
-  Widget build(BuildContext context) {
-    for (var i = 0; i < children.length; i++) {
-      int _flex = flex.asMap().containsKey(i) ? flex[i] : 1;
-
-      items.add(Expanded(flex: _flex, child: children[i]));
-      if (i < children.length - 1) {
-        items.add(const SizedBox(width: 16));
-      }
-    }
-
-    return Row(
-      children: items.toList(),
-    );
-  }
-}
-
-class AppCardTabs extends StatelessWidget {
-  const AppCardTabs(
-      {Key? key,
-      required this.children,
-      required this.length,
-      required this.tabNames,
-      this.uppercase = true})
-      : super(key: key);
-
-  final bool uppercase;
-  final int length;
-  final List<Widget> children;
-  final List<String> tabNames;
-
-  Tab _tab(String text) {
-    String _label = uppercase ? text.toUpperCase() : text;
-    return Tab(
-      child: Text(
-        _label,
-        style: GoogleFonts.barlowCondensed(
-            fontSize: 16, fontWeight: FontWeight.bold),
-      ),
-    );
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return AppCard(mt: 24, pl: 0, pr: 0, pt: 0, pb: 0, children: [
-      DefaultTabController(
-        length: 3,
-        child: Container(
-          height: 400,
-          child: Column(
-            children: [
-              DecoratedBox(
-                // color: AppColors.secondary_99,
-                decoration: BoxDecoration(
-                  color: AppColors.secondary_99.withOpacity(0.5),
-                  border: const Border(
-                      bottom:
-                          BorderSide(color: AppColors.secondary_90, width: 2)),
-                ),
-                child: TabBar(
-                  labelColor: AppColors.secondary_30,
-                  indicatorColor: AppColors.orange,
-                  padding: const EdgeInsets.symmetric(horizontal: 0),
-                  tabs: tabNames.map((name) {
-                    return _tab(name);
-                  }).toList(),
-                ),
-              ),
-              Expanded(
-                  child: TabBarView(
-                children: children,
-              ))
-            ],
-          ),
-        ),
-      )
-    ]);
-  }
 }
