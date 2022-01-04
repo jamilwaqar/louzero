dad397bc24472b7706f5787018f179bf07f161ad
Josh C, UX Developer <josh.coolman@singlemindconsulting.com>
2022-01-03 17:49:21 -0800

    add prop

--- lib/ui/page/job/models/dropdown_item.dart
+++ lib/ui/page/job/models/dropdown_item.dart
@@ -3,9 +3,11 @@ import 'package:louzero/ui/page/job/models/supplier.dart';
 class DropdownItem {
   final String name;
   final String? value;
+  final dynamic data;
 
   const DropdownItem({
     required this.name,
     required this.value,
+    this.data,
   });
 }
