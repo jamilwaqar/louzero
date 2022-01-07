508f2060260495fa7aaea0136029f9705dfd0c50
Mark Austen <austenmark8@gmail.com>
2022-01-07 16:05:38 +0100

    change password, reactivate dialogs

--- lib/ui/page/account/account.dart
+++ lib/ui/page/account/account.dart
@@ -6,17 +6,20 @@ import 'package:louzero/common/app_avatar.dart';
 import 'package:louzero/common/app_button.dart';
 import 'package:louzero/common/app_card.dart';
 import 'package:louzero/common/app_image.dart';
-import 'package:louzero/common/app_row_flex.dart';
+import 'package:louzero/common/app_input_text.dart';
 import 'package:louzero/common/app_text_body.dart';
+import 'package:louzero/common/app_text_header.dart';
 import 'package:louzero/controller/constant/colors.dart';
 import 'package:louzero/controller/get/company_controller.dart';
 import 'package:louzero/controller/state/auth_manager.dart';
 import 'package:louzero/controller/utils.dart';
 import 'package:louzero/models/company_models.dart';
+import 'package:louzero/ui/page/account/edit_account.dart';
 import 'package:louzero/ui/page/app_base_scaffold.dart';
 import 'package:louzero/ui/page/company/add_company.dart';
 import 'package:louzero/ui/page/company/company.dart';
 import 'package:louzero/ui/widget/buttons/top_left_button.dart';
+import 'package:simple_rich_text/simple_rich_text.dart';
 
 class MyAccountPage extends GetWidget<CompanyController> {
   const MyAccountPage({Key? key}) : super(key: key);
@@ -38,124 +41,139 @@ class MyAccountPage extends GetWidget<CompanyController> {
     return Column(
       children: [
         _accountInfo(),
-        const SizedBox(height: 24),
-        AppCard(
-          radius: 16,
-          children: [
-            GetBuilder<CompanyController>(
-                builder: (_) => ListView.builder(
-                    padding: EdgeInsets.zero,
-                    shrinkWrap: true,
-                    physics: const NeverScrollableScrollPhysics(),
-                    itemCount: controller.companies.length,
-                    itemBuilder: (context, index) {
-                      CompanyModel model = controller.companies[index];
-                      return Container(
-                        color: index.isOdd
-                            ? AppColors.oddItemColor
-                            : AppColors.evenItemColor,
-                        height: 65,
-                        child: AppRowFlex(
-                            flex: const [1, 5, 1, 3, 0],
-                            align: CrossAxisAlignment.center,
-                            mb: 0,
-                            children: [
-                              _companyAvatar(model),
-                              AppTextBody(
-                                model.name,
-                                color: AppColors.dark_2,
-                                bold: true,
-                              ),
-                              Container(
-                                width: 10,
-                                height: 10,
-                                decoration: const BoxDecoration(
-                                    shape: BoxShape.circle,
-                                    color: AppColors.medium_2),
-                              ),
-                              AppTextBody(
-                                model.status.label,
-                              ),
-                              AppButton(
-                                margin: const EdgeInsets.only(right: 8),
-                                label: 'SWITCH TO',
-                                color: AppColors.dark_1,
-                                colorText: AppColors.darkest,
-                                primary: false,
-                                onPressed: () {},
-                              ),
-                              PopupMenuButton(
-                                  padding: EdgeInsets.zero,
-                                  offset: const Offset(0, 40),
-                                  onSelected: (value) {
-                                    controller.company = model;
-                                    if (value == 0) {
-                                      Get.to(() => CompanyPage(),
-                                          arguments: model);
-                                    } else if (value == 1) {
-                                      Get.to(() => const AddCompanyPage());
-                                    }
-                                  },
-                                  elevation: 2,
-                                  shape: OutlineInputBorder(
-                                      borderRadius: BorderRadius.circular(8),
-                                      borderSide: const BorderSide(
-                                          color: AppColors.medium_2, width: 0)),
-                                  child: const Icon(Icons.more_vert),
-                                  itemBuilder: (context) => [
-                                        PopupMenuItem(
-                                          child: SizedBox(
-                                            width: 200,
-                                            height: 50,
-                                            child: Row(
-                                              children: const [
-                                                Icon(
-                                                  Icons.check,
-                                                  color: AppColors.icon,
-                                                ),
-                                                SizedBox(width: 10),
-                                                Text("View Company",
-                                                    style: TextStyle(
-                                                      color: AppColors.icon,
-                                                      fontWeight:
-                                                          FontWeight.w400,
-                                                      fontSize: 16,
-                                                    )),
-                                              ],
-                                            ),
-                                          ),
-                                          value: 0,
-                                        ),
-                                        PopupMenuItem(
-                                          child: SizedBox(
-                                            width: 200,
-                                            height: 60,
-                                            child: Row(
-                                              children: const [
-                                                Icon(
-                                                  Icons.edit,
-                                                  color: AppColors.icon,
-                                                ),
-                                                SizedBox(width: 10),
-                                                Text("Edit Company",
-                                                    style: TextStyle(
-                                                      color: AppColors.icon,
-                                                      fontWeight:
-                                                          FontWeight.w400,
-                                                      fontSize: 16,
-                                                    )),
-                                              ],
-                                            ),
-                                          ),
-                                          value: 1,
-                                        ),
-                                      ]),
-                            ]),
-                      );
-                    }))
-          ],
-        ),
+        _companies(),
       ],
+    );
+  }
+
+  Widget _companies() {
+    return AppCard(
+      radius: 16,
+      children: [
+        GetBuilder<CompanyController>(
+            builder: (_) => ListView.builder(
+                padding: EdgeInsets.zero,
+                shrinkWrap: true,
+                physics: const NeverScrollableScrollPhysics(),
+                itemCount: controller.companies.length,
+                itemBuilder: (context, index) => _companyItem(index))),
+        const SizedBox(height: 32),
+        AppAddButton("Add Company", onPressed: () {
+          Get.to(()=> const AddCompanyPage());
+        }),
+      ],
+    );
+  }
+
+  Widget _companyItem(int index) {
+    CompanyModel model = controller.companies[index];
+    return Container(
+      color: index.isOdd
+          ? AppColors.oddItemColor
+          : AppColors.evenItemColor,
+      height: 65,
+      child: Row(
+          children: [
+            const SizedBox(width: 24),
+            _companyAvatar(model),
+            const SizedBox(width: 24),
+            Expanded(
+              child: AppTextBody(
+                model.name,
+                color: AppColors.dark_2,
+                bold: true,
+              ),
+            ),
+            Container(
+              width: 10,
+              height: 10,
+              decoration: const BoxDecoration(
+                  shape: BoxShape.circle,
+                  color: AppColors.medium_2),
+            ),
+            const SizedBox(width: 8),
+            Expanded(
+              child: AppTextBody(
+                model.status.label,
+              ),
+            ),
+            AppButton(
+              margin: const EdgeInsets.only(right: 8),
+              label: 'SWITCH TO',
+              color: AppColors.dark_1,
+              colorText: AppColors.darkest,
+              primary: false,
+              onPressed: () {},
+            ),
+            const SizedBox(width: 24),
+            PopupMenuButton(
+                padding: EdgeInsets.zero,
+                offset: const Offset(0, 40),
+                onSelected: (value) {
+                  controller.company = model;
+                  if (value == 0) {
+                    Get.to(() => CompanyPage(),
+                        arguments: model);
+                  } else if (value == 1) {
+                    Get.to(() => const AddCompanyPage());
+                  }
+                },
+                elevation: 2,
+                shape: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(8),
+                    borderSide: const BorderSide(
+                        color: AppColors.medium_2, width: 0)),
+                child: const Icon(Icons.more_vert),
+                itemBuilder: (context) => [
+                  PopupMenuItem(
+                    child: SizedBox(
+                      width: 200,
+                      height: 50,
+                      child: Row(
+                        children: const [
+                          Icon(
+                            Icons.check,
+                            color: AppColors.icon,
+                          ),
+                          SizedBox(width: 10),
+                          Text("View Company",
+                              style: TextStyle(
+                                color: AppColors.icon,
+                                fontWeight:
+                                FontWeight.w400,
+                                fontSize: 16,
+                              )),
+                        ],
+                      ),
+                    ),
+                    value: 0,
+                  ),
+                  PopupMenuItem(
+                    child: SizedBox(
+                      width: 200,
+                      height: 60,
+                      child: Row(
+                        children: const [
+                          Icon(
+                            Icons.edit,
+                            color: AppColors.icon,
+                          ),
+                          SizedBox(width: 10),
+                          Text("Edit Company",
+                              style: TextStyle(
+                                color: AppColors.icon,
+                                fontWeight:
+                                FontWeight.w400,
+                                fontSize: 16,
+                              )),
+                        ],
+                      ),
+                    ),
+                    value: 1,
+                  ),
+                ]),
+            const SizedBox(width: 24),
+          ]),
     );
   }
 
@@ -201,6 +219,7 @@ class MyAccountPage extends GetWidget<CompanyController> {
       pt: 0,
       pb: 0,
       pr: 0,
+      mb: 24,
       children: [
         SizedBox(
           height: 432,
@@ -233,7 +252,7 @@ class MyAccountPage extends GetWidget<CompanyController> {
                             TopLeftButton(
                                 onPressed: () {
                                   // // Get.find<CustomerController>().customerModel = customerModel;
-                                  // Get.to(() => AddCustomerPage(model: customerModel,));
+                                  Get.to(() => EditAccountPage());
                                 },
                                 iconData: Icons.edit),
                           ],
@@ -277,7 +296,9 @@ class MyAccountPage extends GetWidget<CompanyController> {
                               AuthManager.userModel!.serviceAddress),
                           const Expanded(child: SizedBox()),
                           AppAddButton('Change Password',
-                              iconData: Icons.lock_open, onPressed: () {}),
+                              iconData: Icons.lock_open, onPressed: () {
+                                _showAlertDialog();
+                              }),
                           const SizedBox(height: 24),
                         ],
                       ),
@@ -340,6 +361,95 @@ class MyAccountPage extends GetWidget<CompanyController> {
               style: TextStyles.labelM.copyWith(color: AppColors.lightest)),
         )
       ],
+    );
+  }
+
+  _showAlertDialog() {
+    Dialog errorDialog = Dialog(
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(24.0)), //this right here
+      child: Container(
+        width: 360.0,
+        padding: const EdgeInsets.all(24),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+            const AppTextHeader('Change Password', size: 24, alignLeft: true,),
+            const SizedBox(height: 24),
+            const AppInputText(label: 'Current Password', mb: 24),
+            const AppInputText(label: 'New Password', mb: 24),
+            const AppInputText(label: 'Conform New Password', mb: 24),
+            Align(
+              alignment: Alignment.centerRight,
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.end,
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  AppButton(label: 'Cancel', primary: false, onPressed: () {
+                    Get.back();
+                  },),
+                  const SizedBox(width: 16),
+                  AppButton(label: 'Save', onPressed: () {
+                    Get.back();
+                  },),
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+    showDialog(
+      context: Get.context!,
+      builder: (BuildContext context) {
+        return errorDialog;
+      },
+    );
+  }
+
+  _showReactivateDialog() {
+    String _desc = 'To reactivate *Old Cancelled Company*, please email us *help@evosus.com* and we’ll get back to you as soon as possible';
+    Dialog errorDialog = Dialog(
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(24.0)), //this right here
+      child: Container(
+        width: 360.0,
+        padding: const EdgeInsets.all(24),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+            const AppTextHeader('Reactivate Company', size: 24, alignLeft: true,),
+            const SizedBox(height: 24),
+            SimpleRichText(
+              _desc,
+              style: TextStyles.titleS,
+            ),
+            const SizedBox(height: 24),
+            Align(
+              alignment: Alignment.centerRight,
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.end,
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  AppButton(label: 'Cancel', primary: false, onPressed: () {
+                    Get.back();
+                  },),
+                  const SizedBox(width: 16),
+                  AppButton(label: 'Email', onPressed: () {
+                    Get.back();
+                  },),
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+
+    // show the dialog
+    showDialog(
+      context: Get.context!,
+      builder: (BuildContext context) {
+        return errorDialog;
+      },
     );
   }
 }
