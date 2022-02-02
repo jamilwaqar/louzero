c690e5a2dcb705985fd22268d570bab9da9aba61
Mark Austen <austenmark8@gmail.com>
2022-02-02 21:51:27 +0100

    common tests: app_add_button, app_button

--- test/common_test/app_button_test.dart
+++ test/common_test/app_button_test.dart
@@ -1,21 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:louzero/common/app_add_button.dart';
+import 'package:louzero/common/app_button.dart';
 
 void main() {
-  testWidgets('App Add Button', (WidgetTester tester) async {
+  testWidgets('App Button', (WidgetTester tester) async {
 
     var pressed = false;
     const key = Key('App Button');
 
-    final widget = AppAddButton('App Add Button', key: key, onPressed: () => pressed = true);
+    final widget = AppButton(label: 'App Button', key: key, onPressed: () => pressed = true);
 
     await tester.pumpWidget(MaterialApp(
       home: Center(
         child: widget,
       ),
     ));
-    expect(find.text('App Add Button'), findsOneWidget);
+    expect(find.text('App Button'), findsOneWidget);
     await tester.tap(find.byKey(key));
     await tester.pump();
     expect(pressed, equals(true));
--- test/common_test/app_icon_button_test.dart
+++ test/common_test/app_icon_button_test.dart
@@ -1,21 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:louzero/common/app_add_button.dart';
+import 'package:louzero/common/app_icon_button.dart';
 
 void main() {
-  testWidgets('App Add Button', (WidgetTester tester) async {
+  testWidgets('App Icon Button', (WidgetTester tester) async {
 
     var pressed = false;
-    const key = Key('App Button');
+    const key = Key('App Icon Button');
 
-    final widget = AppAddButton('App Add Button', key: key, onPressed: () => pressed = true);
+    final widget = AppIconButton(key: key, onTap: () => pressed = true);
 
     await tester.pumpWidget(MaterialApp(
       home: Center(
         child: widget,
       ),
     ));
-    expect(find.text('App Add Button'), findsOneWidget);
+    expect(find.byIcon(Icons.close), findsOneWidget);
     await tester.tap(find.byKey(key));
     await tester.pump();
     expect(pressed, equals(true));
