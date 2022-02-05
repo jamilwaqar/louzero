721c8e0d117ec1b1865285c4ba3e15e7ffcb6150
Josh C, UX Developer <josh.coolman@singlemindconsulting.com>
2022-02-05 12:12:59 -0800

    update design (temp)

--- lib/ui/page/customer/customer.dart
+++ lib/ui/page/customer/customer.dart
@@ -54,111 +54,39 @@ class CustomerProfilePage extends GetWidget<CustomerController> {
     return GridView.count(
       physics: const NeverScrollableScrollPhysics(),
       crossAxisCount: 2,
-      crossAxisSpacing: 24,
-      mainAxisSpacing: 24,
+      crossAxisSpacing: 8,
+      mainAxisSpacing: 8,
       shrinkWrap: true,
       children: itemList,
-      childAspectRatio: 2.4 / 1,
+      childAspectRatio: 16 / 9,
     );
   }
 
   Widget _categoryItem(CustomerCategory category) {
-    int count = 0;
-    switch (category) {
-      case CustomerCategory.jobs:
-        break;
-      case CustomerCategory.siteProfiles:
-        count = controller.customerModel!.siteProfiles.length;
-        break;
-      case CustomerCategory.contacts:
-        break;
-      case CustomerCategory.pictures:
-        break;
-      case CustomerCategory.notes:
-        break;
-      case CustomerCategory.subAccounts:
-        break;
-    }
-    return InkWell(
+    return GestureDetector(
       onTap: () {
-        switch (category) {
-          case CustomerCategory.jobs:
-            break;
-          case CustomerCategory.siteProfiles:
-            count = controller.customerModel!.siteProfiles.length;
-            Get.to(() => CustomerSiteProfilePage(
-                controller.customerModel!.siteProfiles,
-                customerId: controller.customerModel!.objectId));
-            break;
-          case CustomerCategory.contacts:
-            break;
-          case CustomerCategory.pictures:
-            break;
-          case CustomerCategory.notes:
-            break;
-          case CustomerCategory.subAccounts:
-            break;
+        if (category == CustomerCategory.siteProfiles) {
+          Get.to(() => CustomerSiteProfilePage(
+              controller.customerModel!.siteProfiles,
+              customerId: controller.customerModel!.objectId));
         }
       },
-      child: Container(
-        alignment: Alignment.topLeft,
-        padding: const EdgeInsets.all(16),
-        decoration: BoxDecoration(
-          border: Border.all(color: AppColors.light_2, width: 1),
-          color: AppColors.lightest,
-          borderRadius: BorderRadius.circular(16),
-        ),
-        child: Row(
-          children: [
-            Align(
-              alignment: Alignment.topLeft,
-              child: Container(
-                width: 64,
-                height: 64,
-                decoration: const BoxDecoration(
-                  shape: BoxShape.circle,
-                  color: AppColors.light_1,
-                ),
-              ),
-            ),
-            const SizedBox(width: 16),
-            Expanded(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Row(
-                    children: [
-                      Expanded(
-                          child: Text(category.title,
-                              style: TextStyles.titleL
-                                  .copyWith(color: AppColors.dark_3))),
-                      Container(
-                        width: 32,
-                        height: 32,
-                        alignment: Alignment.center,
-                        decoration: const BoxDecoration(
-                          shape: BoxShape.circle,
-                          color: AppColors.light_2,
-                        ),
-                        child: Text('$count',
-                            style: const TextStyle(
-                                fontSize: 14,
-                                color: AppColors.dark_3,
-                                fontWeight: FontWeight.bold)),
-                      )
-                    ],
-                  ),
-                  const SizedBox(height: 12),
-                  Text(
-                    category.description,
-                    style: TextStyles.bodyM.copyWith(color: AppColors.dark_3),
-                    maxLines: 2,
-                  ),
-                ],
-              ),
-            ),
-          ],
-        ),
+      child: AppCard(
+        mx: 0,
+        my: 0,
+        py: 16,
+        children: [
+          RowSplit(
+            align: 'center',
+            left: Text(category.title, style: AppStyles.headerDialog),
+            right: const Text('0'),
+          ),
+          const AppDivider(mt: 8),
+          Text(
+            category.description,
+            style: AppStyles.bodyLarge,
+          ),
+        ],
       ),
     );
   }
